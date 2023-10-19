e730ee781eaad3dd7fde728ee6db2f3fb3a39939
root <20CS021@charusat.edu.in>
2023-10-19 07:56:08 +0000

    Added the get_func_stack

---
+++ tinygrad/our_work/get_func_stack.py
@@ -0,0 +1,18 @@
+import inspect
+import sys
+from typing import Optional, List
+import faulthandler 
+
+faulthandler.enable()
+
+def generate_stack(self, ignore_function: List= ['generate_stack'],count: int = 10, counter:bool = False):
+  for i in list(reversed(inspect.stack())):
+    if ignore_function and i.function not in ignore_function:
+      print(f"Function = {i.function} called from {i.filename} at line number ={i.lineno}")
+    elif ignore_function == None:print(f"Function = {i.function} called from {i.filename} at line number ={i.lineno}")
+  
+    # Determines whether the object we pass has counter as attribute or not
+    if counter:
+      if hasattr(self, 'counter') and i.function == inspect.currentframe().f_code.co_name and self.counter <= count:self.counter += 1;print(f"\nCOUNTER = {self.counter}\n")
+      elif hasattr(self, 'counter') == False: print(f"\n\nThe {type(self)} has no counter, please add it\n\n"); sys.exit(1)
+      elif self.counter > count: sys.exit(1)      
